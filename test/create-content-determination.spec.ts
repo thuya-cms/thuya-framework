8055d26645d8caee3992bcfe7fce8400ce044d12
Geri101 <s.geri92@gmail.com>
2023-05-04 11:35:09 +0200

    Create test for determination

--- test/create-content-determination.spec.ts
+++ test/create-content-determination.spec.ts
@@ -7,7 +7,7 @@ import localContentManagementPersistency from "../content-management/persistency
 import contentDefinitionUtil from "./util/content-definition-util";
 import handlerAccessor from "../content-management/persistency/handler-accessor";
 
-describe("validations when creating content", () => {
+describe("determinations when creating content", () => {
     beforeEach(() => {
         contentDefinitionUtil.defineContentField(new TextContentFieldDefinitionDTO("", "id"));
     });
@@ -18,10 +18,10 @@ describe("validations when creating content", () => {
     });
 
     
-    it("should fail with failing validation", () => {
+    it("should update the value with determination", () => {
         const contentField = new TextContentFieldDefinitionDTO("", "field");
-        contentField.addValidator((data) => {
-            return Result.error(`Validation failed with data ${ data }.`);
+        contentField.addDetermination((data) => {
+            return "Updated " + data;
         });
         contentDefinitionUtil.defineContentField(contentField);
 
@@ -32,7 +32,12 @@ describe("validations when creating content", () => {
         const createContentResult = contentManager.createContent("definition", {
             fieldOne: "data1"
         });
-        should().equal(createContentResult.getIsFailing(), true);
-        should().equal(createContentResult.getMessage(), `Validation failed with data data1.`)
+        should().equal(createContentResult.getIsSuccessful(), true, createContentResult.getMessage());
+        
+        const readContentResult = contentManager.readContent(contentDefinition.getName(), createContentResult.getResult()!);
+        should().equal(readContentResult.getIsSuccessful(), true, readContentResult.getMessage());
+
+        const content = readContentResult.getResult();
+        should().equal(content.fieldOne, "Updated data1");
     });
 });
