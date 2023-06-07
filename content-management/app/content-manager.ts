3d000e113fb1b7eaf9340093798c8ac44394736a
Geri101 <s.geri92@gmail.com>
2023-06-07 23:12:04 +0200

    Enhance content manager with missing CRUD methods

--- content-management/app/content-manager.ts
+++ content-management/app/content-manager.ts
@@ -1,8 +1,11 @@
 import { Result } from "../../common";
 import readContentDefinition from "../domain/usecase/content-definition/read-content-definition";
 import createContent from "../domain/usecase/content/create-content";
+import deleteContent from "../domain/usecase/content/delete-content";
+import listContent from "../domain/usecase/content/list-content";
 import readContent from "../domain/usecase/content/read-content";
 import UnknownContent from "../domain/usecase/content/unknown-content.type";
+import updateContent from "../domain/usecase/content/update-content";
 
 /**
  * Manager for content.
@@ -38,11 +41,47 @@ class ContentManager {
      * @returns result containing the id o the content
      */
     async createContent(contentDefinitionName: string, content: any): Promise<Result<string>> {
-        const createContentDefinitionResult = await readContentDefinition.execute(contentDefinitionName);
-        if (createContentDefinitionResult.getIsFailing())
-            return Result.error(createContentDefinitionResult.getMessage());
+        const readContentDefinitionResult = await readContentDefinition.execute(contentDefinitionName);
+        if (readContentDefinitionResult.getIsFailing())
+            return Result.error(readContentDefinitionResult.getMessage());
 
-        return await createContent.execute(createContentDefinitionResult.getResult()!, content);
+        return await createContent.execute(readContentDefinitionResult.getResult()!, content);
+    }
+
+    /**
+     * Update a content.
+     * 
+     * @param contentDefinitionName name of the content definition
+     * @param content data of the content
+     * @returns result
+     */
+    async updateContent(contentDefinitionName: string, content: UnknownContent): Promise<Result> {
+        const readContentDefinitionResult = await readContentDefinition.execute(contentDefinitionName);
+        if (readContentDefinitionResult.getIsFailing())
+            return Result.error(readContentDefinitionResult.getMessage());
+
+        return await updateContent.execute(readContentDefinitionResult.getResult()!, content);
+    }
+
+    /**
+     * Delete a content.
+     * 
+     * @param contentDefinitionName name of the content definition
+     * @param id id of the content
+     * @returns result
+     */
+    async deleteContent(contentDefinitionName: string, id: string): Promise<Result> {
+        return await deleteContent.execute(contentDefinitionName, id);
+    }
+
+    /**
+     * List all content of a content definition.
+     * 
+     * @param contentDefinitionName name of the content definition
+     * @returns result containing the content list
+     */
+    async listContent(contentDefinitionName: string): Promise<Result<UnknownContent>> {
+        return await listContent.execute(contentDefinitionName);
     }
 }
 
