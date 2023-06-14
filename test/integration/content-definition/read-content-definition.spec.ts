7cad0c316dc9116f9ff86d51bdfafa973b2b85c9
Geri101 <s.geri92@gmail.com>
2023-06-14 09:21:42 +0200

    Add tests for read content definition

---
+++ test/integration/content-definition/read-content-definition.spec.ts
@@ -0,0 +1,30 @@
+import { expect } from "chai";
+import contentDefinitionManager from "../../../content-management/app/content-definition-manager";
+import ContentDefinitionDTO from "../../../content-management/app/dto/content-definition";
+import TextContentFieldDefinitionDTO from "../../../content-management/app/dto/content-field-definition/text-content-field-definition";
+import localContentManagementPersistency from "../../../content-management/persistency/local-content-management-persistency";
+import contentDefinitionUtil from "../helper/content-definition-util";
+
+describe("read content definition", () => {
+    beforeEach(async () => {
+        await contentDefinitionUtil.defineContentField(new TextContentFieldDefinitionDTO("", "id"));
+    });
+
+    afterEach(() => {
+        localContentManagementPersistency.clear();
+    });
+
+    
+    
+    it("should return existing content definition", async () => {
+        await contentDefinitionUtil.defineContent(new ContentDefinitionDTO("", "test-definition"));
+
+        const readResult = await contentDefinitionManager.readContentDefinitionByName("test-definition");
+        expect(readResult.getIsSuccessful(), readResult.getMessage()).to.be.true;
+    });
+    
+    it("should fail for not existing content definition", async () => {
+        const readResult = await contentDefinitionManager.readContentDefinitionByName("test-definition");
+        expect(readResult.getIsFailing()).to.be.true;
+    });
+});
