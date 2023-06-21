43870318287f08b056f7cfb4b70cb81dbb4c201c
Geri101 <s.geri92@gmail.com>
2023-06-21 11:21:04 +0200

    Implement read content definition for express

--- content-management/app/content-definition/express-content-definition.controller.ts
+++ content-management/app/content-definition/express-content-definition.controller.ts
@@ -15,7 +15,8 @@ class ExpressContentDefinitionController implements IController {
     constructor() {
         this.router = Router();
 
-        this.router.post("/content-definition", this.createContentDefinition);
+        this.router.post("/content-definition", this.createContentDefinition.bind(this));
+        this.router.get("/content-definition/:name", this.readContentDefinition.bind(this));
     }
     
     
@@ -31,25 +32,14 @@ class ExpressContentDefinitionController implements IController {
     private async createContentDefinition(request: Request, response: Response): Promise<void> {
         try {
             const expressContentDefinitionDTO: ExpressContentDefinitionDTO = request.body;
-            const contentDefinitionDTO = new ContentDefinitionDTO("", expressContentDefinitionDTO.name);
-            
-            if (expressContentDefinitionDTO.contentFields) {
-                for (const expressContentFieldDTO of expressContentDefinitionDTO.contentFields) {
-                    const readContentFieldDefinitionResult = await contentDefinitionManager.readContentFieldDefinitionByName(expressContentFieldDTO.contentFieldDefinitionName);
-                    if (readContentFieldDefinitionResult.getIsFailing()) {
-                        throw new Error(readContentFieldDefinitionResult.getMessage());
-                    }
-    
-                    contentDefinitionDTO.addContentField(expressContentFieldDTO.name, readContentFieldDefinitionResult.getResult()!, expressContentFieldDTO.options);
-                }
-            }
+            const contentDefinitionDTO = await this.convertExpressToAppDTO(expressContentDefinitionDTO);
     
             const createContentDefinitionResult = await contentDefinitionManager.createContentDefinition(contentDefinitionDTO);
             if (createContentDefinitionResult.getIsFailing()) {
                 throw new Error(createContentDefinitionResult.getMessage());
             }
 
-            response.status(200).json({
+            response.status(201).json({
                 id: createContentDefinitionResult.getResult()!
             });
         }
@@ -59,6 +49,61 @@ class ExpressContentDefinitionController implements IController {
                 message: error.message
             });
         }
+    }
+
+    private async readContentDefinition(request: Request, response: Response): Promise<void> {
+        try {
+            const name: string = request.params.name;
+
+            const readContentDefinitionResult = await contentDefinitionManager.readContentDefinitionByName(name);
+            if (readContentDefinitionResult.getIsFailing()) {
+                throw new Error(readContentDefinitionResult.getMessage());
+            }
+
+            const contentDefinitionDTO = readContentDefinitionResult.getResult()!;
+            const expressContentDefinitionDTO: ExpressContentDefinitionDTO = {
+                id: contentDefinitionDTO.getId(),
+                name: contentDefinitionDTO.getName(),
+                contentFields: []
+            };
+
+            for (const contentField of contentDefinitionDTO.getContentFields()) {
+                expressContentDefinitionDTO.contentFields.push({
+                    contentFieldDefinitionName: contentField.contentFieldDefinition.getName(),
+                    name: contentField.name,
+                    options:{
+                        isRequired: contentField.options.isRequired || false,
+                        isUnique: contentField.options.isUnique || false,
+                        isIndexed: contentField.options.isIndexed || false
+                    }
+                });
+            }
+
+            response.status(200).json(expressContentDefinitionDTO);
+        }
+
+        catch (error: any) {
+            response.status(500).json({
+                message: error.message
+            });
+        }
+    }
+    
+    private async convertExpressToAppDTO(expressContentDefinitionDTO: ExpressContentDefinitionDTO): Promise<ContentDefinitionDTO> {
+        const contentDefinitionDTO = new ContentDefinitionDTO("", expressContentDefinitionDTO.name);
+
+        if (expressContentDefinitionDTO.contentFields) {
+            for (const expressContentFieldDTO of expressContentDefinitionDTO.contentFields) {
+                const readContentFieldDefinitionResult = await contentDefinitionManager.readContentFieldDefinitionByName(expressContentFieldDTO.contentFieldDefinitionName);
+                if (readContentFieldDefinitionResult.getIsFailing()) {
+                    throw new Error(readContentFieldDefinitionResult.getMessage());
+                }
+
+                contentDefinitionDTO.addContentField(expressContentFieldDTO.name, readContentFieldDefinitionResult.getResult()!, expressContentFieldDTO.options);
+            }
+        }
+        
+        return contentDefinitionDTO;
     }
 }
 
