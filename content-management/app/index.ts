c791be0e79db4c65eef25cb70fab5b0a6c750ac8
Gergely Simándi <s.geri92@gmail.com>
2023-08-28 15:32:11 +0200

    Expose content event handler

--- content-management/app/index.ts
+++ content-management/app/index.ts
@@ -1,5 +1,6 @@
 export { default as contentDefinitionManager } from "./content-definition-manager";
 export { default as contentManager } from "./content-manager";
 export { default as ContentProvider } from "./content-provider";
+export { default as contentEventHandler } from "./content-event-handler";
 
 export * from "./dto/index";
